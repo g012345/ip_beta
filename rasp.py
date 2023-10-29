e4f1255a918e8fb9955c5ba50dc69218a511c03d
Egor <egorgrot89136@gmail.com>
2023-10-29 19:22:37 +0300

    test

--- rasp.py
+++ rasp.py
@@ -1,17 +1,8 @@
-schedule_data = []
-current_group = None
+# save this as app.py
+from flask import Flask
 
-with open('расписание2kortom1stlb.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        line = line.strip()
-        if line:
-            schedule_data.append(line)
+app = Flask(__name__)
 
-group_schedules = {}
-for item in schedule_data:
-    if "-" in item:
-        current_group = item
-        group_schedules[current_group] = []
-    else:
-        group_schedules[current_group].append(item)
-
+@app.route("/")
+def hello():
+    return "Hello, World!"
