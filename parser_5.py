616e4164050e345e0219f474ad5ee19c6ca29c29
Egor <egorgrot89136@gmail.com>
2023-12-16 23:23:14 +0300

    automation of information updating
    automatic schedule update every 2 hours

--- parser_5.py
+++ parser_5.py
@@ -1,34 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
+import schedule
+import time
+def my_job():
+    url = "https://rsp.chemk.org/5korp/tomorrow.htm"
+    response = requests.get(url)
 
-url = "https://rsp.chemk.org/5korp/tomorrow.htm"
-response = requests.get(url)
+    if response.status_code == 200:
+        response.encoding = "cp1251"
+        soup = BeautifulSoup(response.text, "html.parser")
+        tr_elements = soup.find_all("tr")
 
-if response.status_code == 200:
-    response.encoding = "cp1251"
-    soup = BeautifulSoup(response.text, "html.parser")
-    tr_elements = soup.find_all("tr")
+        with open("rasp5.txt", "w", encoding="utf-8") as file:
+            for tr in tr_elements[0:]:
+                td_elements = tr.find_all("td")
+                if len(td_elements) >= 6:
+                    file.write(td_elements[0].get_text(strip=True) + "\n")
+                    file.write(td_elements[1].get_text(strip=True) + "\n")
+                    file.write(td_elements[2].get_text(strip=True) + "\n")             
+        with open("rasp5.txt", "a", encoding="utf-8") as file:
+            for tr in tr_elements[0:]:
+                td_elements = tr.find_all("td")
+                if len(td_elements) >= 6:
+                    file.write(td_elements[3].get_text(strip=True) + "\n")
+                    file.write(td_elements[4].get_text(strip=True) + "\n")
+                    file.write(td_elements[5].get_text(strip=True) + "\n")
+    else:
+        print("Ошибка при получении страницы. Код состояния:", response.status_code)
 
-    with open("rasp5.txt", "w", encoding="utf-8") as file:
-        for tr in tr_elements[0:]:
-            td_elements = tr.find_all("td")
-            if len(td_elements) >= 6:
-                file.write(td_elements[0].get_text(strip=True) + "\n")
-                file.write(td_elements[1].get_text(strip=True) + "\n")
-                file.write(td_elements[2].get_text(strip=True) + "\n")             
-    with open("rasp5.txt", "a", encoding="utf-8") as file:
-        for tr in tr_elements[0:]:
-            td_elements = tr.find_all("td")
-            if len(td_elements) >= 6:
-                file.write(td_elements[3].get_text(strip=True) + "\n")
-                file.write(td_elements[4].get_text(strip=True) + "\n")
-                file.write(td_elements[5].get_text(strip=True) + "\n")
-else:
-    print("Ошибка при получении страницы. Код состояния:", response.status_code)
+    with open('rasp5.txt', 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+        non_empty_lines = [line for line in lines if line.strip()]
 
-with open('rasp5.txt', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-    non_empty_lines = [line for line in lines if line.strip()]
+    with open('rasp5.txt', 'w', encoding='utf-8') as file:
+        file.writelines(non_empty_lines)
 
-with open('rasp5.txt', 'w', encoding='utf-8') as file:
-    file.writelines(non_empty_lines)
+schedule.every(2).hours.do(my_job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60) 
